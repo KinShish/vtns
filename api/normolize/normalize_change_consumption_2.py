6d70e3063bd12163f24292e3bba316b6a3d1c13f
DS <dimas999000pro@mail.ru>
2025-05-25 13:40:03 +0300

    создал модель с дообогощенными данными

--- api/normolize/normalize_change_consumption_2.py
+++ api/normolize/normalize_change_consumption_2.py
@@ -2,19 +2,28 @@ import json
 import math
 with open(f'../dataset/dataset_train.json', 'r', encoding='utf-8') as f:
     dataset_train = json.load(f)
-with open(f'../dataset/AntiKrot.json', 'r', encoding='utf-8') as f:
+with open(f'../dataset/dataset_test.json', 'r', encoding='utf-8') as f:
     dataset_test = json.load(f)
 
 def normalize(dataset,type_dataset):
     f_count_5000 = 0
     accounts = []
+    accounts_address=[]
+    accounts_id = []
+    count_c=0
     for account in dataset:
-        # if type_dataset=="test" and not account["isCommercial"]: continue
         a=[]
         sr_count_month=0
         max_count_month=0
         count=0
-        flag=False
+        if type_dataset=="train":
+            if account["buildingType"] != 'Частный':continue
+            # accounts_type.append(account["buildingType"])
+        if type_dataset=="train":
+            if account['address'] in accounts_address:continue
+            if account['accountId'] in accounts_id: continue
+            accounts_address.append(account['address'])
+            accounts_id.append(account['accountId'])
         for month in range(1,13):
             if str(month) not in account["consumption"]:account["consumption"][str(month)]=0
             else:
@@ -26,11 +35,12 @@ def normalize(dataset,type_dataset):
                 count+=1
             if max_count_month<account["consumption"][str(month)]:
                 max_count_month=account["consumption"][str(month)]
+            a.append(account["consumption"][str(month)]/10)
         # if flag:
         #     f_count_5000+=1
         #     continue
         # print(count,account["consumption"],max_count_month)
-        if type_dataset=="train" and not account["isCommercial"] and max_count_month > 100:
+        if type_dataset=="train" and not account["isCommercial"] and max_count_month > 45:
             f_count_5000 += 1
             continue
         sr_count_month = sr_count_month / (count if count else 1)
@@ -40,15 +50,23 @@ def normalize(dataset,type_dataset):
         # for i in range(len(a)):
         #     a[i]=a[i]/max_count_month
         a.append(sr_count_month)
-        # a.append(account["roomsCount"] if "roomsCount" in account else 0)
-        # a.append(account["residentsCount"] if "residentsCount" in account else 0)
-        # a.append(account["totalArea"] if "totalArea" in account else 0)
+        a.append(count)
+        # a.append(accounts_type.index(account["buildingType"]))
+        a.append(account["roomsCount"] if "roomsCount" in account else 0)
+        a.append(account["residentsCount"] if "residentsCount" in account else 0)
+        a.append(account["totalArea"] if "totalArea" in account else 0)
+        # if type_dataset == "train" and not account["isCommercial"] and max_count_month > 100:
+        #     a.append(1)
+        #     a.append(0)
+        # else:
         a.append(int(account["isCommercial"]))
         a.append(int(not account["isCommercial"]))
+        if account["isCommercial"]:
+            count_c+=1
         accounts.append(a)
     with open(f'data_{type_dataset}_change.json', 'w', encoding='utf-8') as f:
         json.dump(accounts, f, ensure_ascii=False)
-    print(f_count_5000, len(dataset))
+    print(count_c, len(dataset),count_c/len(accounts))
 
 normalize(dataset_train,'train')
 normalize(dataset_test,'test')
--- api/train/train_change_consumption_2.py
+++ api/train/train_change_consumption_2.py
@@ -9,10 +9,10 @@ with open('../normolize/data_train_change.json', 'r', encoding='utf-8') as f:
 with open('../normolize/data_test_change.json', 'r', encoding='utf-8') as f:
     dataset_test = json.load(f)
 
-column_names = [ 'CH1','CH2','CH3','CH4','CH5','CH6','CH7','CH8','CH9','CH10','CH11','SR','C', 'F']#,"SR","RO","RE","TA"
+column_names = [ 'C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12','CH1','CH2','CH3','CH4','CH5','CH6','CH7','CH8','CH9','CH10','CH11','CP','R','P','A','SR','C', 'F']
 dataset_train = pd.DataFrame(dataset_train, columns=column_names)
 y_train = dataset_train[['C', 'F']]
-x_train = dataset_train.drop(['C', 'F'],axis=1)
+x_train = dataset_train.drop(['C', 'F','SR'],axis=1)
 mean = tf.reduce_mean(x_train, axis=0)
 std = tf.math.reduce_std(x_train, axis=0)
 x_train = (x_train - mean) / std
@@ -21,7 +21,7 @@ print(x_train)
 
 dataset_test = pd.DataFrame(dataset_test, columns=column_names)
 y_test = dataset_test[['C', 'F']]
-x_test = dataset_test.drop(['C', 'F'],axis=1)
+x_test = dataset_test.drop(['C', 'F','SR'],axis=1)
 mean = tf.reduce_mean(x_test, axis=0)
 std = tf.math.reduce_std(x_test, axis=0)
 x_test = (x_test - mean) / std
@@ -30,18 +30,18 @@ x_test = (x_test - mean) / std
 
 def build_model():
     model = keras.Sequential([
-        keras.layers.Dense(128,input_shape=(x_train.shape[1], ), activation='relu'),
+        keras.layers.Dense(256,input_shape=(x_train.shape[1], ), activation='relu'),
         keras.layers.Dense(64, activation='relu'),
         keras.layers.Dense(2, activation='sigmoid')
     ])
 
-    model.compile(optimizer='adam', loss="binary_focal_crossentropy", metrics=['accuracy'])
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.0001), loss="binary_focal_crossentropy", metrics=['accuracy'])
     return model
 
 model = build_model()
 model.summary()
 # print(y_train)
-history = model.fit(x_train, y_train, epochs=50,validation_split=0.1)
+history = model.fit(x_train, y_train, epochs=50,validation_split=0.2)
 
 # print(x_test)
 # print(y_test)
@@ -50,6 +50,8 @@ history = model.fit(x_train, y_train, epochs=50,validation_split=0.1)
 
 test_predictions = model.predict(x_test).flatten()
 # print(test_predictions[0],test_predictions[1],len(test_predictions))
+
+
 def get_percent(type,y_test):
     count = 0
     all = len(y_test)
@@ -68,8 +70,7 @@ def get_percent(type,y_test):
 get_percent("C",y_test)
 get_percent("F",y_test)
 get_percent(0,y_test)
-
-model.save('../model/change.h5')
+model.save('../model/change_2.h5')
 
         # print(test_predictions[i], x_test[i], y_test[i], abs(y_test[i] - test_predictions[i]))
 
