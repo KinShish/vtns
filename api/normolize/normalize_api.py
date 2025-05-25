5f3850e93837b115c68f5848ae3b2ca3c9ff855c
DS <dimas999000pro@mail.ru>
2025-05-25 03:12:24 +0300

    Добавили парсер, обработку нового типа документа и парсер

--- api/normolize/normalize_api.py
+++ api/normolize/normalize_api.py
@@ -22,6 +22,28 @@ def normalize(dataset):
         accounts.append(a)
     return accounts
 
+def normalize_commerce(dataset):
+    accounts = []
+    for account in dataset:
+        a=[]
+        sr_count_month=0
+        count=0
+        for month in range(1,13):
+            if str(month) not in account["consumption"]:account["consumption"][str(month)]=0
+            else:
+                account["consumption"][str(month)]/= 100
+            if account["consumption"][str(month)]>0:
+                sr_count_month+=account["consumption"][str(month)]
+                count+=1
+        sr_count_month = sr_count_month / (count if count else 1)
+        for month in range(1,12):
+            razn=account["consumption"][str(month+1)]-account["consumption"][str(month)]
+            a.append(razn)
+        a.append(sr_count_month)
+        a.append(account["accountId"])
+        accounts.append(a)
+    return accounts
+
 
 
 
--- api/server.py
+++ api/server.py
@@ -3,7 +3,10 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from normolize import normalize_api
 from train import predict
+from flask import send_from_directory
+from PIL import Image
 import os
+import io
 import json
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -16,7 +19,6 @@ def allowed_file(filename):
     filename.rsplit('.', 1)[1].lower() in {'json'}
 @app.route('/load/dataset/chek',methods=['POST'])
 def load_dataset_chek():
-  print(request.files)
   if 'file' not in request.files:
     return "Не могу прочитать файл"
   file = request.files['file']
@@ -36,10 +38,45 @@ def load_dataset_chek():
     return dataset_train
   return "error"
 
+@app.route('/load/dataset/update/commerce',methods=['POST'])
+def load_dataset_commerce():
+  if 'file' not in request.files:
+    return "Не могу прочитать файл"
+  file = request.files['file']
+  if file.filename == '':
+    return "Нет выбранного файла"
+  if file and allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    with open(app.config['UPLOAD_FOLDER'] + '/dataset_test.json', 'r', encoding='utf-8') as f:
+      dataset_train = json.load(f)
+    dataset = normalize_api.normalize_commerce(dataset_train)
+    percent_accounts = predict.predict_commerce(dataset)
+    for i in range(len(percent_accounts)):
+      if percent_accounts[i][1] == dataset_train[i]['accountId']:
+        dataset_train[i]['isCommercial'] = percent_accounts[i][0]
+        dataset_train[i]['aproxima'] = percent_accounts[i][2]
+    return dataset_train
+  return "error"
+
+@app.route('/uploads/account/<id>',methods=['GET'])
+def get_account_photo(id):
+  if not os.path.exists(f"upload/{id}"): return []
+  list_files = os.listdir(f"upload/{id}")
+  return list_files
+
+@app.route('/uploads/account/<id>/<name>',methods=['GET'])
+def download_file(id,name):
+  print(app.config["UPLOAD_FOLDER"],id)
+  return send_from_directory(f'upload/{id}', name)
+
+app.add_url_rule(
+    "/uploads/account/<id>", endpoint="download_file", build_only=True
+)
 @app.route('/')
 def hello_world():
 
   return 'Hello, World!'
 
 if __name__ == '__main__':
-  app.run(debug=True, host='192.168.197.104')
+  app.run(debug=True, host='10.2.0.233', port=80)
